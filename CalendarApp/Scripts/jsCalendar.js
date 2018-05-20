4af904401450d9f91ea41888fad3c95d4c9e62fb
Juan José Romero Maradiaga <juanjo.rm00@gmail.com>
2018-05-20 12:40:15 -0600

    Main structure

---
+++ CalendarApp/Scripts/jsCalendar.js
@@ -0,0 +1,47 @@
+﻿var Cal = function (divId, dateToSetup, displayDays) {
+
+    //Store div id
+    this.divId = divId;
+
+    // Days of week, starting on Sunday
+    this.DaysOfWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+    // Months, stating on January
+    this.Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    // Set the current month, year
+    var d = new Date(dateToSetup);
+
+    this.currMonth = d.getMonth();
+    this.currYear = d.getFullYear();
+    this.currDay = d.getDate();
+    this.SetUpDate = dateToSetup;
+    this.DaysToDisplay = displayDays;
+
+};
+
+// Show current month
+Cal.prototype.showcurr = function() {
+  this.showMonth(this.currYear, this.currMonth, this.DaysToDisplay, this.SetUpDate);
+};
+
+// Show month (year, month)
+Cal.prototype.showMonth = function (y, m, days, inpdate) {
+
+};
+
+// On Load of the window
+window.onload = function () {
+
+    // Start calendar
+    var txtCalendar = Date.parse(getId("startDate").value);
+    var days = getId("numberDays").value;
+    var ObjectCalendar = new Cal("divCal", new Date(txtCalendar), days);
+
+    ObjectCalendar.showcurr();
+}
+
+// Get element by id
+function getId(id) {
+  return document.getElementById(id);
+}
